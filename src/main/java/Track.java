28524d709459df3f16ef33754bce4a914bdad0b5
BigRorz <rory.wl.edmonds@gmail.com>
2023-02-27 11:43:46 +0000

    Initial Commit

---
+++ src/main/java/Track.java
@@ -0,0 +1,66 @@
+/**
+ * Represents a point in space and time, recorded by a GPS sensor.
+ *
+ * @author Rory Edmonds
+ */
+public class Track 
+{
+  // Constructor; (stub)
+  public Track()
+  {
+
+  }
+
+  public Track(String filename)
+  {
+
+  }
+
+  // Reads from the file; (stub)
+  public void readFile(String filename)
+  {
+
+  }
+
+  // Adds to a point; (stub)
+  public void add(Point p)
+  {
+
+  }
+
+  // Returns a point;  (stub)
+  public Point get(int index)
+  {
+    return null;
+  }
+
+  // Returns the size; (stub)
+  public int size()
+  {
+    return 0;
+  }
+
+  // Returns the lowest point; (stub)
+  public Point lowestPoint()
+  {
+    return null;
+  }
+
+  // Returns the highest point; (stub)
+  public Point highestPoint()
+  {
+    return null;
+  }
+
+  // Returns the total distance point; (stub)
+  public double totalDistance()
+  {
+    return 0.0;
+  }
+
+  //// Returns the average speed; (stub)
+  public double averageSpeed()
+  {
+    return 0.0;
+  }
+}
